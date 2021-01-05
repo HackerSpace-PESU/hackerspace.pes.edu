903c672357dcc9eee580c27a8c5c7142e7b25afd
Anirudh H M <anihm136@gmail.com>
2021-01-05 23:23:14 +0530

    Should fix static files

--- hackerspace_site/settings.py
+++ hackerspace_site/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR, "allstatic")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [Path(BASE_DIR, "hackerspace_site", "static")]
--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,2 +1,3 @@
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --noinput
