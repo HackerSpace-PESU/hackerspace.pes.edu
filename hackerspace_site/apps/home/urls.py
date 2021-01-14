23891907b020502c77bfb2001e14e21af0420f18
Daniel <saldanha.dam@gmail.com>
2021-01-14 16:55:31 +0530

    Front-end completed

--- hackerspace_site/apps/home/urls.py
+++ hackerspace_site/apps/home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
 from .views import HomePageView
+from .views import AboutUsPageView
 
-urlpatterns = [path("", HomePageView.as_view(), name="home")]
+urlpatterns =   [path("", HomePageView.as_view(), name="home"),
+                path("About-Us", AboutUsPageView.as_view(), name="aboutus")]
--- hackerspace_site/settings.py
+++ hackerspace_site/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
-from pathlib import Path
 
 import dj_database_url
+
+from pathlib import Path
 
 from .martor_settings import *
 
@@ -24,18 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-DEBUG = os.environ.get("DEBUG", False)
-
+DEBUG = True
 SECRET_KEY = "jisoo ssi saranghaeyo"
-
-if not DEBUG:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "hackerspace.local",
-    "hackerspace-pesu.herokuapp.com",
+    "hackerspace-pesu.herokuapp.com"
 ]
 
 # Application definition
@@ -57,7 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -91,13 +87,12 @@ WSGI_APPLICATION = "hackerspace_site.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-    "default": dj_database_url.config(conn_max_age=600)
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    # "default": dj_database_url.config(conn_max_age=600)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
