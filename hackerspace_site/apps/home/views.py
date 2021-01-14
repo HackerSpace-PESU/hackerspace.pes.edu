3763df5987af4de0c8495aa685df8a525403b107
Daniel Lester Saldanha <54536136+DAN-329@users.noreply.github.com>
2021-01-14 17:40:00 +0530

    Front-end completed (#7)
    Co-authored-by: Daniel <saldanha.dam@gmail.com>
    Completed basic frontend for unveiling

--- hackerspace_site/apps/home/views.py
+++ hackerspace_site/apps/home/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home/home.html"
+
+class AboutUsPageView(TemplateView):
+    template_name="home/aboutus.html"
