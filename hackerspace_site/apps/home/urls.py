21c5e223fbc520572a11589f2a4f862200e5c170
Daniel <saldanha.dam@gmail.com>
2021-01-25 01:06:20 +0530

    SpaceJam website version-1

--- hackerspace_site/apps/home/urls.py
+++ hackerspace_site/apps/home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import AboutUsPageView, HomePageView
+from .views import SpaceJamPageView
 
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("About-Us", AboutUsPageView.as_view(), name="aboutus"),
+    path("SpaceJam", SpaceJamPageView.as_view(), name="spacejam")
 ]
