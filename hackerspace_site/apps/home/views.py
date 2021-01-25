c66f5183aaaf4395334f3f1fbe70a6de9c2bdcfe
Daniel Lester Saldanha <54536136+DAN-329@users.noreply.github.com>
2021-01-25 09:20:01 +0530

    SpaceJam website version-1 (#10)
    Co-authored-by: Daniel <saldanha.dam@gmail.com>
    Add initial spacejam website with partner branding

--- hackerspace_site/apps/home/views.py
+++ hackerspace_site/apps/home/views.py
@@ -7,3 +7,6 @@ class HomePageView(TemplateView):
 
 class AboutUsPageView(TemplateView):
     template_name = "home/aboutus.html"
+
+class SpaceJamPageView(TemplateView):
+    template_name="home/spacejam.html"
